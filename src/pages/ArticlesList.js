5974c27478f38d9edec7f00d916cae09e173e8cc
rashmij1290 <rashmij1290@gmail.com>
2022-09-23 22:28:23 +0530

    changed-ArticlesList

--- src/pages/ArticlesList.js
+++ src/pages/ArticlesList.js
@@ -4,7 +4,7 @@ import articles from "./article-content";
 
 const ArticlesList = () => {
   return (
-    <><h1 className="sm:text-4xl text-2xl font-bold mt-6 text-gray-900">
+    <></><h1 className="sm:text-4xl text-2xl font-bold mt-6 text-gray-900">
           Article List
       </h1><div className="container py-4 mx-auto">
               <div className="flex flex-wrap -m-4">
